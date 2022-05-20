dcc04fe28d00b246b16e6b9cc08159bf8ccbc6e9
Angel Barajas <Barajasb15@gmail.com>
2022-05-19 19:33:30 -0500

    add: 3 vistas estaticas: Introduccion, Agradeciminetos, Nosotros

---
+++ BiblioWeb/Controllers/StaticController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BiblioWeb.Controllers
+{
+    public class StaticController : Controller
+    {
+        public IActionResult Introduccion()
+        {
+            return View();
+        }
+        public IActionResult Nosotros() { 
+            return View();
+        }
+        public IActionResult Agradecimientos() {
+            return View();
+        }
+    }
+}
