08681949595ce6a695f6c7abdec8bcf398b0e05c
Angel Barajas <Barajasb15@gmail.com>
2022-05-27 20:19:17 -0500

    add: Manejo de sesiones

--- BiblioWeb/Controllers/HomeController.cs
+++ BiblioWeb/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
 ﻿using BiblioWeb.Clases;
 using BiblioWeb.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace BiblioWeb.Controllers
@@ -19,11 +23,13 @@ namespace BiblioWeb.Controllers
             _logger = logger;
         }
         [HttpGet]
+        [Authorize(Roles = "Usuario")]
         public IActionResult Ventas(int id) { 
 
             return View(new UsuariosCLS().MostrarUnLibro(id));
         }
         [HttpPost]
+        [Authorize(Roles = "Usuario")]
         public IActionResult Ventas(string id) {
             new UsuariosCLS().RegistrarPedido(int.Parse(id));
             return RedirectToAction("Carrito","Home");
@@ -33,12 +39,24 @@ namespace BiblioWeb.Controllers
             return View();
         }   
         [HttpPost]
-        public IActionResult Login(TbUsuario user)
+        public async Task<IActionResult> LoginAsync(TbUsuario user)
         {
             string mensaje = new UsuariosCLS().Iniciar_Sesion(user);
             if (mensaje != "Correcto") {
                 return View();
             }
+            var claims = new List<Claim>
+                    {
+                        new Claim("Usuario", user.Correo),
+                        new Claim("Contraseña", user.Contraseña),
+
+                    };
+
+            claims.Add(new Claim(ClaimTypes.Role, "Usuario"));
+
+            var claimsIdentityAdmin = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentityAdmin));
             return RedirectToAction("Menu");
             
         }
@@ -53,22 +71,25 @@ namespace BiblioWeb.Controllers
             return View();
         }
         [HttpGet]
+        [Authorize(Roles = "Usuario")]
         public IActionResult Carrito() {
             ViewBag.Precio = new UsuariosCLS().PrecioTotal();
             ViewBag.ListaID = new UsuariosCLS().GetIdPedido();
             return View(new UsuariosCLS().MostrarPedidos());
         }
         [HttpPost]
+        [Authorize(Roles = "Usuario")]
         public IActionResult Eliminar(int idEliminarPedido) {
             new UsuariosCLS().EliminarPedido(idEliminarPedido);
             return RedirectToAction("Carrito");
         }
         [HttpPost]
+        [Authorize(Roles = "Usuario")]
         public IActionResult Compra() {
             new UsuariosCLS().Comprar();
             return RedirectToAction("Carrito");
         }
-        
+        [Authorize(Roles = "Usuario")]
         public IActionResult Menu() {
 
             return View(new UsuariosCLS().MostrarLibros());
