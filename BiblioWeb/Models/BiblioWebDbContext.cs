ae592fb5157ff398897266c1bdfa2cd3a98868ac
Angel Barajas <Barajasb15@gmail.com>
2022-05-28 02:34:07 -0500

    add: string de conexion en la nube

--- BiblioWeb/Models/BiblioWebDbContext.cs
+++ BiblioWeb/Models/BiblioWebDbContext.cs
@@ -29,8 +29,7 @@ namespace BiblioWeb.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-A9VAJ5L;Initial Catalog=BiblioWebDb;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Server=tcp:biblioweb.database.windows.net,1433;Initial Catalog=BiblioWeb;Persist Security Info=False;User ID=administrador;Password=BiblioWeb1.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
