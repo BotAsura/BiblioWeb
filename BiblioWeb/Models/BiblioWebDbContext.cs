b1801a9592ad081f4a471dc9d04d7f051ccfbf5d
Angel Barajas <Barajasb15@gmail.com>
2022-05-28 02:15:14 -0500

    add: Extras

--- BiblioWeb/Models/BiblioWebDbContext.cs
+++ BiblioWeb/Models/BiblioWebDbContext.cs
@@ -30,7 +30,7 @@ namespace BiblioWeb.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Data Source=Asura;Initial Catalog=BiblioWebDb;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-A9VAJ5L;Initial Catalog=BiblioWebDb;Integrated Security=True");
             }
         }
 
